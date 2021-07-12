29770efc98a6a6bbc04e22d36ef17766c9bd96db
Maicon Peixinho <maiconpeixinho@globo.com>
2021-07-12 16:45:33 -0300

    Adding pdf consumer and save on the root folder

---
+++ cpi.py
@@ -0,0 +1,26 @@
+
+from bs4 import BeautifulSoup
+
+import os
+import requests
+import urllib
+
+html = requests.get("https://legis.senado.leg.br/comissoes/docsRecCPI?codcol=2441").content
+
+soup = BeautifulSoup(html, 'html.parser')
+
+links = []
+
+for table in soup.find_all("table", class_="table"):
+    for anchor in table.find_all('a'):
+        links.append({"name":anchor.contents[0], "link":anchor['href']})
+
+print(links[0])
+
+r = requests.get(links[0]["link"], stream = True)
+
+with open("%s.%s" % (links[0]["name"],"pdf"),"wb") as pdf:
+    for chunk in r.iter_content(chunk_size=1024):
+         # writing one chunk at a time to pdf file
+         if chunk:
+             pdf.write(chunk)
